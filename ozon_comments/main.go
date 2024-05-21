a469d1610bafad90c301b0cefc1bb73bd4f66867
hud0shnik <c4nsero@mail.ru>
2024-05-21 16:09:09 +0300

    ozon_comments: add graphql
    Попытался (надеюсь удачно) добавить GraphQL

--- ozon_comments/main.go
+++ ozon_comments/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"comments/graph"
 	"comments/internal/config"
+	"net/http"
+	"os"
+	"time"
 
-	"github.com/sirupsen/logrus"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	log "github.com/sirupsen/logrus"
 )
+
+const defaultPort = "8080"
 
 func main() {
 
+	log.SetFormatter(&log.JSONFormatter{
+		TimestampFormat: time.DateTime,
+	})
+
 	err := config.Init()
 	if err != nil {
-		logrus.Fatalf("InitConfig error: %v", err)
+		log.Fatalf("InitConfig error: %v", err)
 	}
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
