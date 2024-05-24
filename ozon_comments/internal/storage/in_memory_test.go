4af41b43740bb24b004aa170472460dbf35d8608
hud0shnik <c4nsero@mail.ru>
2024-05-24 12:45:02 +0300

    ozon_comments: add getPostById tests
    Добавил тесты для функционала поиска поста по айди

--- ozon_comments/internal/storage/in_memory_test.go
+++ ozon_comments/internal/storage/in_memory_test.go
@@ -64,3 +64,72 @@ func TestAddPost(t *testing.T) {
 	}
 
 }
+
+func TestGetPostById(t *testing.T) {
+
+	normalPost := model.Post{
+		Author:          "normalAuthor",
+		Header:          "Normal Header",
+		Content:         "Normal content",
+		CommentsAllowed: true,
+	}
+
+	s := InMemoryStorage{
+		Post:    NewInMemoryPostStorage(20),
+		Comment: NewInMemoryCommentStorage(20),
+	}
+
+	res, err := s.Post.AddPost(normalPost)
+	if err != nil {
+		t.Errorf("Can not add test post: %v", err)
+	}
+
+	tests := []struct {
+		name    string
+		input   int
+		want    *model.Post
+		wantErr bool
+	}{
+		{
+			name:    "Normal post check",
+			input:   res.ID,
+			want:    &normalPost,
+			wantErr: false,
+		},
+		{
+			name:    "Id: -1 check",
+			input:   -1,
+			want:    &model.Post{},
+			wantErr: true,
+		},
+		{
+			name:    "Id: 4294967296 check",
+			input:   4294967296,
+			want:    &model.Post{},
+			wantErr: true,
+		},
+		{
+			name:    "Id: 0 check",
+			input:   0,
+			want:    &model.Post{},
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			found, err := s.Post.GetPostById(test.input)
+			if err != nil && !test.wantErr {
+				t.Errorf("GetPostById() error: %v, want error: %v", err, test.wantErr)
+			}
+
+			if !isPostsEqual(found, *test.want) {
+				t.Errorf("GetPostById() result: %v, want: %v", found, test.want)
+			}
+
+		})
+
+	}
+
+}
--- ozon_comments/internal/storage/postgres_test.go
+++ ozon_comments/internal/storage/postgres_test.go
@@ -60,3 +60,74 @@ func TestSavePostToPostgres(t *testing.T) {
 	}
 
 }
+
+func TestGetPostFromPostgresById(t *testing.T) {
+
+	godotenv.Load("../../.env")
+
+	normalPost := model.Post{
+		Author:          "normalAuthor",
+		Header:          "Normal Header",
+		Content:         "Normal content",
+		CommentsAllowed: true,
+	}
+
+	db, err := GetPostgresDB()
+	if err != nil {
+		t.Errorf("can not connect to Postgres: %v", err)
+	}
+
+	res, err := SavePostToPostgres(db, normalPost)
+	if err != nil {
+		t.Errorf("Can not add test post: %v", err)
+	}
+
+	tests := []struct {
+		name    string
+		input   int
+		want    *model.Post
+		wantErr bool
+	}{
+		{
+			name:    "Normal post check",
+			input:   res.ID,
+			want:    &normalPost,
+			wantErr: false,
+		},
+		{
+			name:    "Id: -1 check",
+			input:   -1,
+			want:    &model.Post{},
+			wantErr: true,
+		},
+		{
+			name:    "Id: 4294967296 check",
+			input:   4294967296,
+			want:    &model.Post{},
+			wantErr: true,
+		},
+		{
+			name:    "Id: 0 check",
+			input:   0,
+			want:    &model.Post{},
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			found, err := GetPostFromPostgresById(db, &test.input)
+			if err != nil && !test.wantErr {
+				t.Errorf("GetPostById() error: %v, want error: %v", err, test.wantErr)
+			}
+
+			if !isPostsEqual(found, *test.want) {
+				t.Errorf("GetPostById() result: %v, want: %v", found, test.want)
+			}
+
+		})
+
+	}
+
+}
